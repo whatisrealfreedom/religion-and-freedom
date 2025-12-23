0b8e727bd30d1ea0f7e165a8af42b165b8704bb5
Freedom <mydigisaffron@gmail.com>
2025-12-23 15:07:34 +0100

    feat: Add React app structure with routing

---
+++ frontend/src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Chapter from './pages/Chapter';
+import Resources from './pages/Resources';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gradient-to-br from-gray-50 to-blue-50">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/chapter/:id" element={<Chapter />} />
+            <Route path="/resources" element={<Resources />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
